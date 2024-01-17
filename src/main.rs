cb50502fc26cb70407f8813eca27b3c16ee1ba8c
deboard <john.deboard@gmail.com>
2024-01-16 19:39:04 -0500

    Initial checkin. Started from cpal record wav example.

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use clap::Parser;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::{FromSample, Sample};
+use std::fs::File;
+use std::io::BufWriter;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
+use ctrlc;
+
+fn main() -> Result<(), anyhow::Error> {
+    println!("wavloop");
+
+    let running = Arc::new(AtomicBool::new(true));
+    let r = running.clone();
+
+    ctrlc::set_handler(move || {
+        r.store(false, Ordering::SeqCst);
+    }).expect("Error setting Ctrl-C handler");
+
+    let host = cpal::default_host();
+    
+    let device = host.default_input_device().unwrap();
+
+    let dname =  device.name();
+    match dname {
+        Ok(n) => {
+            println!("Input device: {n}");
+        },
+        Err(e) => {},
+    }
+
+    let config = device
+        .default_input_config()
+        .expect("Failed to get default input config");
+    println!("Default input config: {:?}", config);
+
+    const PATH: &str = "/tmp/recorded.wav";
+
+    let spec = wav_spec_from_config(&config);
+    let writer = hound::WavWriter::create(PATH, spec);
+    let writer = Arc::new(Mutex::new(Some(writer)));
+
+    println!("Begin recording...");
+
+    // Run the input stream on a separate thread.
+    let writer_2 = writer.clone();
+
+    let err_fn = move |err| {
+        eprintln!("an error occurred on stream: {}", err);
+    };
+
+    let stream = match config.sample_format() {
+        cpal::SampleFormat::I8 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
+            err_fn,
+            None,
+        ),
+        cpal::SampleFormat::I16 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
+            err_fn,
+            None,
+        ),
+        cpal::SampleFormat::I32 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
+            err_fn,
+            None,
+        ),
+        cpal::SampleFormat::F32 => device.build_input_stream(
+            &config.into(),
+            move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
+            err_fn,
+            None,
+        ),
+        sample_format => {
+            return Err(anyhow::Error::msg(format!(
+                "Unsupported sample format '{sample_format}'"
+            )))
+        }
+    };
+
+   // let sp = stream.play();
+
+    //match stream.play().unwrap() {
+    //}
+
+    // Let recording go for roughly three seconds.
+    //std::thread::sleep(std::time::Duration::from_secs(3));
+    
+    println!("Waiting for Ctrl-C...");
+    while running.load(Ordering::SeqCst) {}
+    println!("Got it! Exiting...");
+
+    drop(stream);
+    writer.lock().unwrap().take().unwrap().ok();
+    println!("Recording {} complete!", PATH);
+    Ok(())
+
+}
+
+fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
+    if format.is_float() {
+        hound::SampleFormat::Float
+    } else {
+        hound::SampleFormat::Int
+    }
+}
+
+fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
+    hound::WavSpec {
+        channels: config.channels() as _,
+        sample_rate: config.sample_rate().0 as _,
+        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
+        sample_format: sample_format(config.sample_format()),
+    }
+}
+
+type WavWriterHandle = Arc<Mutex<Option<Result<hound::WavWriter<BufWriter<File>>, hound::Error>>>>;
+
+fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
+where
+    T: Sample,
+    U: Sample + hound::Sample + FromSample<T>,
+{
+    if let Ok(mut guard) = writer.try_lock() {
+        if let Some(writer) = guard.as_mut() {
+            
+            for &sample in input.iter() {
+                let sample: U = U::from_sample(sample);
+                match writer {
+                    Ok(wr) => {
+                        wr.write_sample(sample).ok();
+                    },
+                    Err(e) => {
+                        println!("{e}");
+                    },
+                }
+            }
+
+        }
+    }
+}
+
